2b0d6f824d483d62f031430c77f777767b066058
Seifeddine Touj <seifeddine.touj1@gmail.com>
2024-06-21 18:32:53 +0200

    Config & Logging

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,11 +1,21 @@
 package main
 
 import (
-	"log"
+	"flag"
+	"log/slog"
 	"net/http"
+	"os"
 )
 
 func main() {
+
+    addr := flag.String("addr", ":4000", "HTTP network address")
+    flag.Parse()
+
+    logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+        AddSource: true,
+    }))
+
 
     mux := http.NewServeMux()
 
@@ -17,8 +27,9 @@ func main() {
     mux.HandleFunc("GET /snippet/create", snippetCreate)
     mux.HandleFunc("POST /snippet/create", snippetCreatePost)
 
-    log.Print("starting server on :4000")
+    logger.Info("starting server ","addr",*addr)
     
-    err := http.ListenAndServe(":4000", mux)
-    log.Fatal(err)
+    err := http.ListenAndServe(*addr, mux)
+    logger.Error(err.Error())
+    os.Exit(1)
 }
