36f48e2cef500266ec3cd4d1c0d21f6fd46e8075
Seifeddine Touj <seifeddine.touj1@gmail.com>
2024-06-21 17:51:34 +0200

    Adding visuals

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,14 +1,36 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
-    "strconv"
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
-    w.Header().Add("Server", "Go")
-    w.Write([]byte("Hello from Snippetbox"))
+    // w.Header().Add("Server", "Go")
+    // w.Write([]byte("Hello from Snippetbox"))
+
+	files := []string{
+        "./ui/html/base.tmpl",
+        "./ui/html/pages/home.tmpl",
+		"./ui/html/partials/nav.tmpl",
+    }
+
+	ts,err:=template.ParseFiles(files...)
+	if err!= nil{
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	err =ts.ExecuteTemplate(w,"base",nil)
+	if err!= nil{
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
 }
 
 func snippetView(w http.ResponseWriter, r *http.Request) {
